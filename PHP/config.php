910bd3630d4927696596bcbe3234e0dc8a840531
Elem3ntal <javier08@hotmail.com>
2017-03-24 01:36:43 -0300

    funcion leave a message, envia correo y registra en la bd, alert corregido

--- PHP/config.php
+++ PHP/config.php
@@ -5,4 +5,22 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'qwerasd123');
 define('DB_DATABASE', 'Elem3ntalDevelopment');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+function sendMail($toCopy,$text){
+    try{
+        $message=$text;
+        $subject=explode('@',$toCopy,2)[0].', has send you a message';
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        // More headers
+        $headers .= 'From: Message elem3naldnt@gmail.com'. "\r\n" . 'Cc: ' .$toCopy;
+        mail("javier08@hotmail.com",$subject,$message,$headers);
+        return 1;
+    }
+    catch (Throwable $t) {
+        echo 'error catched!: '.$t->getMessage();
+        return 0;
+    }
+}
 ?>
--- PHP/leaveAMessage.php
+++ PHP/leaveAMessage.php
@@ -4,8 +4,10 @@ $variable = explode(",", $_GET["q"]);
 if($variable[0]!=""){
     $email = mysqli_real_escape_string($db,$variable[0]);
     $text = mysqli_real_escape_string($db,$variable[1]);
+    sendMail($email,$text);
     $sql = "SELECT dejarMensaje('".$email."','".$text."');";
     $result = mysqli_query($db,$sql);
     $row = mysqli_fetch_array($result);
+
 }
 ?>
