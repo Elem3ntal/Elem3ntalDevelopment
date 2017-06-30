cf3bc55ad8bd4d37ff5c7eeb659623bc1959312a
Elem3ntal <javier08@hotmail.com>
2017-06-30 16:08:47 -0400

    Change of website, new html and css

--- OLD/JS/Android.js
+++
@@ -1,6 +0,0 @@
-var ua = navigator.userAgent.toLowerCase();
-var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
-if(isAndroid) {
-    var link = document.getElementById("whatsapp");
-    link.setAttribute("href", "intent://send/+56999402264#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end");
-}
